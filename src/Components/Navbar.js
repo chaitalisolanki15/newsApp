e8721030404e4fd4fb9e92708946cd1a5403456b
Chaitali Solanki <121747353+chaitalisolanki15@users.noreply.github.com>
2024-05-15 20:42:09 +0530

    Done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import {
+    
+    Link
+  } from "react-router-dom";
 
 export class Navbar extends Component {
-    static propTypes = {
-
-    }
+    
 
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">News Hub</a>
+                        <Link className="navbar-brand" to="/">News Hub</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link aria-current="page" to="/general">Home</Link>
+                                </li>
+                                {/* <li className="nav-item">
+                                    <Link to="/about">About</a>
+                                </li> */}
+                                <li className="nav-item">
+                                    <Link to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <Link to="/business">Entertainment</Link>
+                                </li>
+                                {/* <li className="nav-item">
+                                    <Link to="/general">General</Link>
+                                </li> */}
+                                <li className="nav-item">
+                                    <Link to="/health">Health</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/science">Science</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/sports">Sports</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/technology">Technology</Link>
                                 </li>
                                 
                             </ul>
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
+        
           <img src={!imageUrl?"https://bilder1.n-tv.de/img/incoming/crop24896720/7601324875-cImg_16_9-w1200/458472511.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
+              <h5 className="card-title">{title} <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+                {source}</span> </h5>
               <p className="card-text">{description}..</p>
+              <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
               <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
